71eaff8b28582d2dfbe60185469ab2876340c0e0
shalanubale <ubaleshalan1836@gmail.com>
2025-05-09 01:31:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+function toRoman(num) {
+    if (num === 0) return ''; // No Roman numeral for 0
 
-  //your code here
+    const romanMap = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ];
 
+    let result = '';
+    for (let [roman, value] of romanMap) {
+        while (num >= value) {
+            result += roman;
+            num -= value;
+        }
+    }
+
+    return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
+// Example usage:
+console.log(toRoman(14));     // Output: XIV
+console.log(toRoman(798));    // Output: DCCXCVIII
+console.log(toRoman(100000)); // Output: M repeated 100 times
